7b44ca013537f7df1e5888118a9b184b52ffa312
Renan Correia <renansap@gmail.com>
2019-10-09 00:15:10 -0300

    Ajustado limite das telas

--- src/mortalkombat/Player.java
+++ src/mortalkombat/Player.java
@@ -13,95 +13,47 @@ import javax.swing.JLabel;
  *
  * @author Ksa
  */
-public class Player extends JLabel {
-
-    public int x = 0, y = 0;
+public class Player extends JLabel{
+    
+        public int x = 70, y = 180;    
     ImageIcon walkL;
     ImageIcon walkR;
     ImageIcon walkU;
     ImageIcon walkD;
     ImageIcon stoppedD; //Parada Direita
     ImageIcon stoppedE;//Parada Esquerda
-
-    public void setup() {
-        setText("12");
-
-        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-
-        walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+    public void setup(){
+        //setText("12");
+        
+        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(70, 127, Image.SCALE_DEFAULT));
+        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(70, 127, Image.SCALE_DEFAULT));
+        
+        walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));  
         walkL = new ImageIcon(new ImageIcon(getClass().getResource("c_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-
+        
         setBounds(x, y, 90, 127);
         setIcon(walkR);
     }
-
-//    private boolean excedeuAreaDaTela(int posX, int posY) {
-//        return posX < 0 || posX + 180 > 750 || posY < 0 || posY + 90 > 700;
-//    }
-    public void move() {
-
-        if (x > 0) { //####renan#### implementado limite de largura da esquerda
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            x = 0;
-            setBounds(x, y, 90, 127);
-
-        }
-        if (x < 696) { //####renan#### implementado limite de largura da direita
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            x = 696;
-            setBounds(x, y, 90, 127);
-
-        }
-
-        if (y > 0) { //####renan#### implementado limite de altura pra cima
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            y = 0;
-            setBounds(x, y, 90, 127);
-
-        }
-        if (y < 432) { //####renan#### implementado limite de largura pra baixo
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            y = 432;
-            setBounds(x, y, 90, 127);
-
-        }
-
-//        setBounds(x, y, 90, 127);
-//        System.out.println("X"+x);
-//        System.out.println("Y"+y);
+    
+    
+    
+    public void move(){
+        setBounds(x, y, 90, 127);
     }
-
-    public void setIconRight() {
+    
+    public void setIconRight(){
         setIcon(walkR);
     }
-
-    public void setIconLeft() {
+    
+    public void setIconLeft(){
         setIcon(walkL);
     }
-
-    public void setIconStoppedD() {
+    
+    public void setIconStoppedD(){
         setIcon(stoppedD);
     }
-
-    public void setIconStoppedE() {
+        public void setIconStoppedE(){
         setIcon(stoppedE);
     }
-
+    
 }
