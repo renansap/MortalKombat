3e71d229009e55cc094cc56614ef7a3fedc88a21
Renan Correia <renansap@gmail.com>
2019-10-24 23:50:27 -0300

    testes criando servidor

--- src/mortalkombat/Player.java
+++ src/mortalkombat/Player.java
@@ -5,102 +5,128 @@
  */
 package mortalkombat;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
  * @author Ksa
  */
-public class Player extends JLabel {
-
-    public int x = 0, y = 0;
-    ImageIcon walkL;
-    ImageIcon walkR;
-    ImageIcon walkU;
-    ImageIcon walkD;
-    ImageIcon stoppedD; //Parada Direita
-    ImageIcon stoppedE;//Parada Esquerda
-
-    public void setup() {
-        setText("12");
-
-        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-        walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-        walkL = new ImageIcon(new ImageIcon(getClass().getResource("c_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-
-        setBounds(x, y, 90, 127);
-        setIcon(walkR);
-    }
-
-//    private boolean excedeuAreaDaTela(int posX, int posY) {
-//        return posX < 0 || posX + 180 > 750 || posY < 0 || posY + 90 > 700;
-//    }
-    public void move() {
-
-        if (x > 0) { //####renan#### implementado limite de largura da esquerda
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            x = 0;
-            setBounds(x, y, 90, 127);
-
-        }
-        if (x < 696) { //####renan#### implementado limite de largura da direita
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            x = 696;
-            setBounds(x, y, 90, 127);
-
+public class Player {
+    
+    public int x=70, y=130, w= 90, h = 127, id, punch = 0,pontos = 0;
+    public char lado = 'R';
+    
+    public Socket conexao;
+    public PrintWriter out;
+    
+    public Player(Socket socket) {
+        
+        
+        Random r = new Random();
+        this.id = r.nextInt();
+        
+        this.conexao = socket;
+        
+        try {
+            out = new PrintWriter(this.conexao.getOutputStream(),true);
+        } catch (IOException ex) {
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        if (y > 0) { //####renan#### implementado limite de altura pra cima
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
+}
 
-        } else {
-            y = 0;
-            setBounds(x, y, 90, 127);
-
-        }
-        if (y < 432) { //####renan#### implementado limite de largura pra baixo
-            setBounds(x, y, 90, 127);
-            System.out.println("X" + x);
-            System.out.println("Y" + y);
-
-        } else {
-            y = 432;
-            setBounds(x, y, 90, 127);
-
-        }
-
+//    public int x = 0, y = 0;
+//    ImageIcon walkL;
+//    ImageIcon walkR;
+//    ImageIcon walkU;
+//    ImageIcon walkD;
+//    ImageIcon stoppedD; //Parada Direita
+//    ImageIcon stoppedE;//Parada Esquerda
+//
+//    public void setup() {
+//        setText("12");
+//
+//        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkL = new ImageIcon(new ImageIcon(getClass().getResource("c_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//
 //        setBounds(x, y, 90, 127);
-//        System.out.println("X"+x);
-//        System.out.println("Y"+y);
-    }
-
-    public void setIconRight() {
-        setIcon(walkR);
-    }
-
-    public void setIconLeft() {
-        setIcon(walkL);
-    }
-
-    public void setIconStoppedD() {
-        setIcon(stoppedD);
-    }
-
-    public void setIconStoppedE() {
-        setIcon(stoppedE);
-    }
+//        setIcon(walkR);
+//    }
+//
+////    private boolean excedeuAreaDaTela(int posX, int posY) {
+////        return posX < 0 || posX + 180 > 750 || posY < 0 || posY + 90 > 700;
+////    }
+//    public void move() {
+//
+//        if (x > 0) { //####renan#### implementado limite de largura da esquerda
+//            setBounds(x, y, 90, 127);
+//            System.out.println("X" + x);
+//            System.out.println("Y" + y);
+//
+//        } else {
+//            x = 0;
+//            setBounds(x, y, 90, 127);
+//
+//        }
+//        if (x < 696) { //####renan#### implementado limite de largura da direita
+//            setBounds(x, y, 90, 127);
+//            System.out.println("X" + x);
+//            System.out.println("Y" + y);
+//
+//        } else {
+//            x = 696;
+//            setBounds(x, y, 90, 127);
+//
+//        }
+//
+//        if (y > 0) { //####renan#### implementado limite de altura pra cima
+//            setBounds(x, y, 90, 127);
+//            System.out.println("X" + x);
+//            System.out.println("Y" + y);
+//
+//        } else {
+//            y = 0;
+//            setBounds(x, y, 90, 127);
+//
+//        }
+//        if (y < 432) { //####renan#### implementado limite de largura pra baixo
+//            setBounds(x, y, 90, 127);
+//            System.out.println("X" + x);
+//            System.out.println("Y" + y);
+//
+//        } else {
+//            y = 432;
+//            setBounds(x, y, 90, 127);
+//
+//        }
+//
+////        setBounds(x, y, 90, 127);
+////        System.out.println("X"+x);
+////        System.out.println("Y"+y);
+//    }
+//
+//    public void setIconRight() {
+//        setIcon(walkR);
+//    }
+//
+//    public void setIconLeft() {
+//        setIcon(walkL);
+//    }
+//
+//    public void setIconStoppedD() {
+//        setIcon(stoppedD);
+//    }
+//
+//    public void setIconStoppedE() {
+//        setIcon(stoppedE);
+//    }
 
 }
---
+++ src/mortalkombat/PlayerThread.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mortalkombat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.awt.event.KeyEvent;
+
+
+/**
+ *
+ * @author Ksa
+ */
+public class PlayerThread implements Runnable {
+    Boolean keyRight = false, keyLeft = false, keyUp = false, keyDown = false;
+
+    public Socket socket;
+    Player player;
+    public static final int SPEED = 8;
+
+    public PlayerThread(Player player, Socket socket) {
+        this.player = player;
+        this.socket = socket;
+
+    }
+  
+    @Override
+    public void run() {
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+
+            String command = "";
+
+            while (true) {
+                if (!(command = in.readLine()).equals("exit")) {
+                    if (command.equals("PR_R")) {
+                        player.x += SPEED;
+                        player.lado = 'R';
+                    }
+                    if (command.equals("PR_L") && player.x >= 0) {
+                        player.x -= SPEED;
+                        player.lado = 'L';
+                    }
+                    if (command.equals("PR_U") && player.y >= 0) {
+                        player.y -= SPEED;
+                    }
+                    if (command.equals("PR_D")) {
+                        player.y += SPEED;
+                    }
+                    if (command.equals("PUNCH")) {
+                        player.punch = 1;
+                    }
+
+                }
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PlayerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
